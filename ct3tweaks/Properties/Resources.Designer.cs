b4d13ea41b83fcbc507184dc99e920666979c0e8
Stash <aestheticrice@gmail.com>
2019-04-06 01:05:24 +0300

    Update manifest + add icon

--- ct3tweaks/Properties/Resources.Designer.cs
+++ ct3tweaks/Properties/Resources.Designer.cs
@@ -59,5 +59,25 @@ namespace ct3tweaks.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap car {
+            get {
+                object obj = ResourceManager.GetObject("car", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon ct3tweaks {
+            get {
+                object obj = ResourceManager.GetObject("ct3tweaks", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
