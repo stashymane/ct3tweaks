578555866b9b66cd2fb7a04651eac0d29db87e12
Stash <aestheticrice@gmail.com>
2019-04-05 04:36:08 +0300

    Add folder browser to choose directory

--- ct3tweaks/MainWindow.xaml.cs
+++ ct3tweaks/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ct3tweaks
 {
@@ -24,11 +12,13 @@ namespace ct3tweaks
         {
             InitializeComponent();
             SetAdvancedMode(Properties.Settings.Default.AdvancedMode);
+            SetDirectory(Properties.Settings.Default.Directory);
         }
 
         private void SetAdvancedMode(bool advanced)
         {
             Properties.Settings.Default.AdvancedMode = advanced;
+            Properties.Settings.Default.Save();
             if (advanced)
                 this.SettingsModeToggleButton.Content = "Simple mode";
             else
@@ -38,6 +28,44 @@ namespace ct3tweaks
         private void SettingsModeToggleButton_Click(object sender, RoutedEventArgs e)
         {
             SetAdvancedMode(!Properties.Settings.Default.AdvancedMode);
+        }
+
+        private void SetDirectory(string directory)
+        {
+            if (File.Exists(directory + "/CT3.exe"))
+            {
+                Properties.Settings.Default.Directory = directory;
+                Properties.Settings.Default.Save();
+            }
+            else if (!directory.Equals(""))
+            {
+                MessageBoxResult result = MessageBox.Show("Game not found in specified directory!\n" + directory,
+                                          "Error",
+                                          MessageBoxButton.OK,
+                                          MessageBoxImage.Asterisk);
+                Properties.Settings.Default.Directory = "";
+                Properties.Settings.Default.Save();
+            }
+            UpdateDirectoryString();
+        }
+
+        private void UpdateDirectoryString()
+        {
+            string location = Properties.Settings.Default.Directory;
+            if (!location.Equals(""))
+                this.GameLocationString.Text = location;
+            else
+                this.GameLocationString.Text = "Not set";
+        }
+
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                dialog.ShowDialog();
+                if (!dialog.SelectedPath.Equals(""))
+                    SetDirectory(dialog.SelectedPath);
+            }
         }
     }
 }
