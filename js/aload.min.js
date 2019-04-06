2a34fe648f7023aa1684f05ce291f11fc4004ca6
Stash <aestheticrice@gmail.com>
2019-04-07 01:49:23 +0300

    Initial commit

---
+++ js/aload.min.js
@@ -0,0 +1,1 @@
+function aload(t){"use strict";var e="data-aload";return t=t||window.document.querySelectorAll("["+e+"]"),void 0===t.length&&(t=[t]),[].forEach.call(t,function(t){t["LINK"!==t.tagName?"src":"href"]=t.getAttribute(e),t.removeAttribute(e)}),t}
